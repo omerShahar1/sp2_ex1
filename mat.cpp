1c804c5cef878b13cdb559ed69010be8ddb792f2
omerShahar1 <93426230+omerShahar1@users.noreply.github.com>
2022-03-11 00:30:51 +0200

    Add files via upload

--- mat.cpp
+++ mat.cpp
@@ -1,35 +1,29 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include "mat.hpp"
 using namespace std;
 
 
 namespace ariel 
 {
-    void addSymbols(char **matrix, int length, int width, int count, char symbol)
-    {// function will add the symbol to the matrix in 2 raws and 2 columns (top, button, right and left)
-        for(int i=count; i<length-count; i++)
-        {
-            matrix[i][count] = symbol;
-            matrix[i][width - 1 - count] = symbol;
-        }
-
-        for(int i=count; i<width-count; i++)
-        {
-            matrix[count][i] = symbol;
-            matrix[length - 1 - count][i] = symbol;
-
-        }
-    }
-
-    string toString(char **matrix, int length, int width)
+    string toString(vector<vector<char>> matrix, char symbol1, int length, int width)
     {//function will make the matrix to string
-        string ans = "";
-        for(int i=0; i< length; i++)
+        string ans(1, symbol1);
+        bool firstTime = true;
+
+        for(int i=0; i < length; i++)
         {
             for(int j=0; j<width; j++)
             {
-                ans += matrix[i][j];
+                if (!firstTime)
+                {
+                    ans += string(1, matrix[i][j]);
+                }
+                else
+                {
+                    firstTime = false;
+                }
             }
             ans += "\n";
         }
@@ -39,58 +33,77 @@ namespace ariel
 
     string mat(int width, int length, char symbol1, char symbol2) //create the matrix, call other functions, delete matrix and return the string answer.
     {
-        if(width < 0 || length < 0)
+        int i=0;
+        int count=0;
+        if(width < 0 || length < 0) // in case of negative size
         {
             throw invalid_argument( "mat size can't be negative" );
         }
 
-        if(width%2 == 0 || length%2 == 0)
+        if(width%2 == 0 || length%2 == 0) // in case of ilegal size
         {
             throw invalid_argument( "mat size is always add" );
         }
         
-        if(width == 1)
+        if(width == 1) // in case we only have 1 row to retuen
         {
-            string row = "";
-            for(int i=0; i<length; i++)
-        {
-            row += symbol1;
-            row += "\n";
-        }
+            string row(1, symbol1);
+            for(i=1; i<length; i++)
+            {
+                row.append("\n");
+                row.append(string(1, symbol1));
+            }
             return row;
         }
 
-        if(length == 1)
+        if(length == 1)  // in case we only have 1 line to return
         {
-            string line = "";
-            for(int i=0; i<width; i++)
-                line += symbol1;
+            string line(1, symbol1);
+            for(i=1; i<width; i++)
+            {
+                line.append(string(1, symbol1));
+            }
             return line;
         }
 
-        char **matrix = new char *[length]; // dynamically create an array of pointers
-        for (int i = 0; i < length; i++)  // dynamically allocate memory of size `width` for each row
-            matrix[i] = new char[width];
-
-        int count=0;
+        
+        vector <vector<char> > matrix(length, vector<char>(width, '0'));
+        count = 0;
         while(length-count-1 != 0 && width-count-1 !=0)
         {
             if(count%2 == 0)
             {
-                addSymbols(matrix, length, width, count, symbol1);
+                for(int i=count; i<length-count; i++)
+                {
+                    matrix[i][count] = symbol1;
+                    matrix[i][width - 1 - count] = symbol1;
+                }
+
+                for(int i=count; i<width-count; i++)
+                {
+                    matrix[count][i] = symbol1;
+                    matrix[length - 1 - count][i] = symbol1;
+
+                }
             }
             else
             {
-                addSymbols(matrix, length, width, count, symbol2);
+                for(int i=count; i<length-count; i++)
+                {
+                    matrix[i][count] = symbol2;
+                    matrix[i][width - 1 - count] = symbol2;
+                }
+
+                for(int i=count; i<width-count; i++)
+                {
+                    matrix[count][i] = symbol2;
+                    matrix[length - 1 - count][i] = symbol2;
+
+                }
             }
             count++;
         }
-        string ans = toString(matrix, length, width);
-
-        for(int i = 0; i < length; i++) // deallocate memory using the `delete[]` operator
-            delete[] matrix[i];
-        delete[] matrix;
-
+        string ans = toString(matrix, symbol1, length, width);
         return ans;
     }
 }
--- mat.hpp
+++ mat.hpp
@@ -5,7 +5,6 @@
 	#define _MAT_HPP_
     namespace ariel 
     { 
-        void addSymbols(char **matrix, int length, int width, int count, char symbol);
         std::string toString(char **matrix, int length, int width);
         std::string mat( int width, int length, char symbol1, char symbol2);
     };
